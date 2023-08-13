714c2ed524baaec0433491d2297b705acb00697a
Angel Lozano <angel_ivan_5@outlook.com>
2023-08-12 23:36:00 -0600

    Compiled with latest quartus version

--- systemverilog/ledBlink/ledBlink.sv
+++ systemverilog/ledBlink/ledBlink.sv
@@ -4,16 +4,16 @@ module ledBlink(
 	output logic pin
 );
 
-	reg [25:0] 	counter = 0;
-	logic 		state = 0;
+	reg [25:0] 	counter = 26'd0;
+	logic 		state   = 26'd0;
 	localparam 	DIVISOR = 26'd50000000;				//2Hz, 50% DC
 	
 	assign pin = state;
 	assign led = state;
 	
 	always_ff @(posedge clk) begin
-		counter <= counter + 1;
-		if (counter == (DIVISOR - 1)) begin
+		counter <= counter + 26'd1;
+		if (counter == (DIVISOR - 26'd1)) begin
 			state <= ~state;
 			counter <= 26'd0;
 		end
