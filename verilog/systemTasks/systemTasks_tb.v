d8ebd7d1e9be61691271ae63a6c6eaf3111a16cb
Angel Lozano <angel_ivan_5@outlook.com>
2023-10-14 01:42:25 -0600

    Finished exercise for displaying in tb

--- verilog/systemTasks/systemTasks_tb.v
+++ verilog/systemTasks/systemTasks_tb.v
@@ -7,14 +7,21 @@ module systemTasks_tb();
 	
 	initial begin
 		#0;
+		x = 8'd1;
+		$display("THIS IS A DISPLAY TASK x=%d \t\t z=%d t=%t", x, z, $time);		//Displayed in active region as integers
+		$monitor("THIS IS A MONITOR TASK x=%b \t z=%b t=%t", x, z, $time);		//Displayed in postpone region in binary radix every value is updated
+		#10;
+		x = 8'd2;
+		$display("THIS IS A DISPLAY TASK x=%c \t z=%c t=%t", x, z, $time);		//Displayed in active region as characters
+		#10;
+		x = 8'd4;
+		$strobe("THIS IS A STROBE  TASK x=%b \t z=%b t=%t", x, z, $time);		//Displayed in postpone region in binary radix
+		#10;
+		x = 8'd8;
+		$strobe("THIS IS A STROBE  TASK x=%H \t z=%H t=%t", x, z, $time);		//Displayed in postpone region in hex radix
+		#10;
 		x = 8'd16;
-		$display("x=%d \t z=%d t=%t", x, z, $time);
-		#10;
-		x = 8'd32;
-		$display("x=%d \t z=%d t=%t", x, z, $time);
-		#10;
-		x = 8'd40;
-		$monitor("x=%d \t z=%d t=%t", x, z, $time);
+		$write("THIS IS A WRITE   TASK x=%H \t z=%H t=%t", x, z, $time);			//Displayed in active region in hex radix without new line
 		#10;
 		
 	end
